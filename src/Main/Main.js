b69ef56eb501d5b4ae1baf84fba670447566a7c5
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-05-13 15:07:05 +0200

    styleContainer

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import style from './Main.module.css'
+import styleContainer from '../Common/Styles/Container.module.css'
+
 
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={style.mainContainer}>
+            <div className={styleContainer.container}>
                 <div className={style.mainGreeting}>
                     <span>Hi There </span>
                     <h1>I am Bohdan Solomchak</h1>
                     <p>Front-end Developer</p>
                 </div>
-                <div className={style.mainPhoto}></div>
+                <div className={style.mainPhoto}>
+                </div>
             </div>
-
         </div>
     );
-};
+}
 
