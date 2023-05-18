5e6d1e7e47ee1758c08aef41873092716dd6a03a
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-05-18 11:30:17 +0200

    projectsEnd

--- src/Projects/Project/Project.js
+++ src/Projects/Project/Project.js
@@ -4,15 +4,16 @@ import style from './Project.module.css'
 export const Project = (props) => {
     return (
         <div className={style.project}>
-            <div>
-                <img className={style.projectImg} src={''} alt={'Project photo'}/>
-                <a href={''}>To look</a>
-                <div className={style.projectInfo}>
-                    <h4 className={style.projectName}>{props.name}</h4>
-                    <p className={style.description}>{props.description}</p>
-                </div>
-
+            <div className={style.imgHref}>
+                <a href={''} title={'Click here'}> <img className={style.projectImg}
+                                   src={props.projectPhoto}
+                                   alt={'Project photo'}/></a>
             </div>
+            <div className={style.containerName}>
+                <h4 className={style.projectName}>{props.name}</h4>
+                <p className={style.description}>{props.description}</p>
+            </div>
+
 
         </div>
 
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Projects.module.css'
 import styleContainer from '../Common/Styles/Container.module.css'
 import {Project} from "./Project/Project";
+import PhotoForProject1 from './ProjectsPhoto/art4.png'
 
 export const Projects = () => {
     return (
@@ -10,8 +11,12 @@ export const Projects = () => {
                 <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                     <h2 className={style.ProjectsTitle}> My projects</h2>
                     <div className={style.projects}>
-                        <Project name={'TodoList'} description={'It is a time management tool'}/>
-                        <Project name={'Social Network'} description={'It is a Social Network'}/>
+                        <Project name={'TodoList'}
+                                 description={'It is a time management tool'}
+                                 projectPhoto={PhotoForProject1}/>
+                        <Project name={'Social Network'}
+                                 description={'It is a Social Network'}
+                                 projectPhoto={PhotoForProject1}/>
 
                     </div>
                 </div>
