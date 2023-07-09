b1bd8da35e8f501551189f6662a75613dfeab790
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-07-09 21:13:56 +0200

    portfolio2Start

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -4,7 +4,6 @@ import styleContainer from '../Common/Styles/Container.module.css'
 import {Skill} from "./Skill/Skill";
 
 
-
 // ray use Skills with Map later
 
 export const Skills = () => {
@@ -15,9 +14,17 @@ export const Skills = () => {
                 <div className={style.skills}>
                     <Skill title={'React'} description={'It is a library'}/>
                     <Skill title={'JS'} description={'It is a programming language'}/>
-                    <Skill title={'HTML'} description={'It is marking'}/>
-                    <Skill title={'CSS'} description={'It is a styles'}/>
-
+                    <Skill title={'HTML5'} description={'It is marking'}/>
+                    <Skill title={'CSS3'} description={'It is a styles'}/>
+                    <Skill title={'TypeScript'} description={'It is...'}/>
+                    <Skill title={'Redux'} description={'It is a styles'}/>
+                    <Skill title={'Storybook'} description={'It is a styles'}/>
+                    <Skill title={'Git'} description={'It is a styles'}/>
+                    <Skill title={'REST API'} description={'It is a styles'}/>
+                    <Skill title={'Axios'} description={'It is a styles'}/>
+                    <Skill title={'Postman'} description={'It is a styles'}/>
+                    <Skill title={'Material UA'} description={'It is a styles'}/>
+                    <Skill title={'Unit Testing'} description={'It is a styles'}/>
                 </div>
             </div>
         </div>
