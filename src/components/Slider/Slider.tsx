3c2873345600c008e2e9f8f062abae56420ba56a
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-02-13 16:17:14 +0100

    TestimonyEnd

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import styled from "styled-components";
+import {FlexWrapper} from "../flexWrapper";
 
 export const Slider = () => {
     return (
         <StyledSlider>
-            <Slide>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dicta dolorum earum ipsa necessitatibus rem.</Text>
-                <Name>@Bogdan</Name>
-            </Slide>
+            <FlexWrapper>
+                <Slide>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dicta dolorum earum ipsa
+                        necessitatibus rem.</Text>
+                    <Name>@Bogdan</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span></span>
+                <span></span>
+                <span></span>
+            </Pagination>
         </StyledSlider>
     );
 };
 
 const StyledSlider = styled.div`
     border: 1px solid red;
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Slide = styled.div`
+    text-align: center;
 `
-const Text = styled.div`
+const Text = styled.p`
+
 `
-const Name = styled.div`
+const Name = styled.span`
+
+`
+
+const Pagination = styled.div`
+    span {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        border-radius: 5px;
+        margin: 5px;
+        background-color: red;
+    }
 `
--- src/loyaut/sections/testimony/testimony.tsx
+++ src/loyaut/sections/testimony/testimony.tsx
@@ -3,18 +3,21 @@ import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/Slider/Slider";
+import {FlexWrapper} from "../../../components/flexWrapper";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
-            <Icon iconId={'codeReact'}/>
-            <Slider/>
+            <FlexWrapper directions={'column'} align={'center'}>
+                <Icon iconId={'codeReact'}/>
+                <Slider/>
+            </FlexWrapper>
         </StyledTestimony>
     );
 };
 
-const StyledTestimony =styled.section `
-min-height: 100vh;
-    background-color: darkgray;
+const StyledTestimony = styled.section`
+    background-color: #d7cbcb;
+    min-height: 40vh;
 `
