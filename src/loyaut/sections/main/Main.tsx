26777ae648100a830a16797222d3b6ed03da3511
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-02-20 17:01:42 +0100

    part2MainStart

--- src/loyaut/sections/main/Main.tsx
+++ src/loyaut/sections/main/Main.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import styled from "styled-components";
 import photo from "../../../assets/images/myPhoto.jpg"
 import {FlexWrapper} from "../../../components/flexWrapper";
+import {Container} from "../../../components/Container";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'}>
+            <Container>
+            <FlexWrapper align={'center'} justify={'space-between'}>
                 <StyledTextBlock>
-                    <span>Hi There</span>
+                    <SmallText>Hi There</SmallText>
                     <StyledMainName>I am Bohdan Solomchak</StyledMainName>
                     <StyledMainTitle>Frontend developer</StyledMainTitle>
                 </StyledTextBlock>
 
                 <Photo src={photo} alt={'main photo'}/>
             </FlexWrapper>
-
-
+            </Container>
         </StyledMain>
     );
 };
@@ -25,14 +26,30 @@ export const Main = () => {
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: lightskyblue;
+    display: flex;
 `
 const StyledTextBlock = styled.div`
 
 `
 const StyledMainTitle = styled.h1`
+    font-weight: 400;
+    font-size: 27px;
+    
+    
 `
 
 const StyledMainName = styled.h2`
+    font-family: 'Josefin Sans',serif;
+    font-weight: 700;
+    font-size: 50px;
+    letter-spacing: 0.05em;
+    
+`
+
+const SmallText = styled.span`
+    font-weight: 400;
+    font-size: 14px;
+
 `
 const Photo = styled.img`
     width: 350px;
