1d526c3c6506413e5f10ef1c61466e7641b9017b
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-02-29 15:46:44 +0100

    part2SloganEnd

--- src/loyaut/sections/slogan/Slogan.tsx
+++ src/loyaut/sections/slogan/Slogan.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
+import {Container} from "../../../components/Container";
+import {FlexWrapper} from "../../../components/flexWrapper";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle>I am ready to Work!</SectionTitle>
-            <Button>Hire my</Button>
+            <Container>
+                <FlexWrapper directions={'column'} align={'center'}>
+                    <SectionTitle>I am ready to Work!</SectionTitle>
+                    <Button>Hire my</Button>
+                </FlexWrapper>
+            </Container>
+
         </StyledSlogan>
     );
 };
