c91283698b659c1fc68bd6740e82c3764f5e032a
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-03-09 21:17:53 +0100

    part3MainEnd

--- src/loyaut/sections/main/Main.tsx
+++ src/loyaut/sections/main/Main.tsx
@@ -4,17 +4,18 @@ import photo from "../../../assets/images/myPhoto.jpg"
 import {FlexWrapper} from "../../../components/flexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={'center'} justify={'space-between'} wrap={'wrap'}>
-                    <StyledTextBlock>
+                <FlexWrapper align={'center'} justify={'space-around'} wrap={'wrap'}>
+                    <div>
                         <SmallText>Hi There</SmallText>
                         <StyledMainName>I am <span>Bohdan Solomchak</span></StyledMainName>
                         <StyledMainTitle>Frontend developer</StyledMainTitle>
-                    </StyledTextBlock>
+                    </div>
                     <PhotoWrapper>
                         <Photo src={photo} alt={'main photo'}/>
                     </PhotoWrapper>
@@ -31,59 +32,10 @@ const StyledMain = styled.section`
     background-color: lightskyblue;
     display: flex;
 `
-const StyledTextBlock = styled.div`
-
-`
-const StyledMainTitle = styled.h1`
-    font-weight: 400;
-    font-size: 27px;
-
-
-`
-
-const StyledMainName = styled.h2`
-    font-family: 'Josefin Sans', serif;
-    font-weight: 700;
-    font-size: 50px;
-    letter-spacing: 0.05em;
-    margin: 10px 0;
-
-    span {
-        position: relative;
-        z-index: 0;
-
-        &::before {
-            content: "";
-            display: inline-block;
-            width: 100%;
-            height: 20px;
-            background-color: ${theme.colors.accent};
-            position: absolute;
-            bottom: 0;
-            z-index: -1;
-        }
-    }
-
-`
-
-const SmallText = styled.span`
-    font-weight: 400;
-    font-size: 14px;
-
-`
-const Photo = styled.img`
-    width: 350px;
-    height: 430px;
-    object-fit: cover;
-    
-    @media ${theme.media.mobile} {
-        width: 310px;
-        height: 380px;
-    }
-`
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
 
     &::before {
         content: "";
@@ -99,6 +51,62 @@ const PhotoWrapper = styled.div`
         @media ${theme.media.mobile} {
             width: 314px;
             height: 414px;
+            top: -17px;
+            left: 24px;
+
         }
+    }
+`
+const StyledMainTitle = styled.h1`
+    ${font({weight: 400, FMax: 27, FMin: 20})}
+
+`
+
+const StyledMainName = styled.h2`
+    ${font({family: "'Josefin Sans', serif",weight: 700, FMax: 50, FMin: 36})}
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+
+
+    span {
+        position: relative;
+        z-index: 0;
+        white-space: nowrap;
+
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+    
+    
+    @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+}
+`
+
+const SmallText = styled.span`
+    font-weight: 400;
+    font-size: 14px;
+
+`
+
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+    margin-right: 20px;
+
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
     }
 `
--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -1,9 +1,19 @@
-type fontPropsType = {
-    family?: string
-    weight?: number
-    color?: string
-    lineHeight?: number
-}
-export const font = ({family, weight, color, lineHeight}: fontPropsType) => {
+import {theme} from "./Theme";
 
+type FontPropsType = {
+    family?: string;
+    weight?: number;
+    color?: string;
+    lineHeight?: number;
+    FMin?: number;
+    FMax?: number;
 }
+export const font = ({family, weight, color, lineHeight, FMin, FMax}: FontPropsType) => `
+    font-family: ${family || 'Poppins'};
+    font-weight: ${weight || 400};
+    color: ${color || theme.colors.font};
+    line-height: ${lineHeight || 1.2};
+
+    font-size: calc( (100vw - 360px) / (1440 - 360) * (${FMax} - ${FMin}) + ${FMin}px);
+
+`
