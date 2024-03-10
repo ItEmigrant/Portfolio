55154810d159a2ec587e480edd9f18fec84e93ac
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-03-10 14:00:14 +0100

    part3SkillsStart

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {Footer} from "./loyaut/footer/Footer";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App" >
             <Header/>
             <Main/>
             <Skills/>
