8c17f9cf068d269b2f408500669a34e40fbd0cea
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-03-11 15:44:47 +0100

    part3SkillsStart1

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,26 +1,32 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h2`
+    ${font({family: "'Josefin Sans', sans-serif", weight: 600, FMax: 36, FMin: 30})}
+
+
     text-align: center;
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 600;
-    font-size: 36px;
     letter-spacing: 5px;
-    position: relative;
     margin-bottom: 90px;
-    
+
+    position: relative;
+
     &::before {
         content: '';
         display: inline-block;
         width: 55px;
         height: 1px;
         background-color: ${theme.colors.MainColor};
-        
+
         position: absolute;
         left: 50%;
         bottom: -30px;
         transform: translateX(-50%);
         
+        @media ${theme.media.mobile} {
+        bottom: -24px;
+    }
+
     }
 `
