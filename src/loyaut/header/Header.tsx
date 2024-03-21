69fc9efb287802e79a0977390b32bbb2f6fc7089
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-03-21 16:11:18 +0100

    part3RefactoringStart

--- src/loyaut/header/Header.tsx
+++ src/loyaut/header/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/flexWrapper";
 import {MenuHeader} from "./MenuHeader";
 import {MenuMobile} from "./MenuMobile";
+import {S} from './Header_Styles'
 
 
 const value = ['Home', 'Skills', 'Works', 'Testimony', 'Contact']
-export const Header = () => {
+export const Header:React.FC = () => {
     return (
-        <StyledHeader>
+        <S.StyledHeader>
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo/>
@@ -18,17 +18,7 @@ export const Header = () => {
                     <MenuMobile value={value}/>
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </S.StyledHeader>
     );
 };
 
-const StyledHeader = styled.header`
-    background-color: rgba(31, 31, 32, 0.9);
-    padding: 20px 0;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 9999;
-
-`
