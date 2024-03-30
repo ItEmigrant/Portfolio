80c48a133a5efae5333b3b945184fbd78817c9ab
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-03-30 20:35:06 +0100

    part4burgerMenuUseState

--- src/loyaut/header/Menu/headerMenu/MenuMobile.tsx
+++ src/loyaut/header/Menu/headerMenu/MenuMobile.tsx
@@ -1,13 +1,18 @@
 import {Menu} from "./Menu";
-import React from "react";
+import React, {useState} from "react";
 import {S} from './HeaderMenu_Styles'
+
 export const MenuMobile: React.FC<{ value: string[] }> = (props: { value: string[] }) => {
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+    const onClickButton = () => {
+        setIsOpenMenu(!isOpenMenu)
+    }
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton isOpen={isOpenMenu} onClick={onClickButton}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={isOpenMenu}>
                 <Menu value={props.value}/>
             </S.MobileMenuPopup>
 
