a033bd9534a338cd2485c5ba5701357cf465c409
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-04-09 18:05:07 +0200

    part4sliderAliseCarouselStartEnd

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import {S} from './Slider_Styles'
+import {S} from './Slider_Styles';
+import '../../styles/slider.css'
 
 type SlidePropsType = {
     text: string
     name: string
 }
 
-const Slide = (props:SlidePropsType ) => {
+const Slide = (props: SlidePropsType) => {
     return <S.Slide>
         <S.Text>{props.text}</S.Text>
         <S.Name>@{props.name}</S.Name>
@@ -34,28 +35,4 @@ export const Slider = () => (
                 items={items}
             />
         </S.StyledSlider>
-    )
-;
-/*import React from 'react';
-import {FlexWrapper} from "../flexWrapper";
-import {S} from './Slider_Styles'
-
-export const Slider: React.FC = () => {
-    return (
-        <S.StyledSlider>
-            <FlexWrapper>
-                <S.Slide>
-                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dicta dolorum earum ipsa
-                        necessitatibus rem.</S.Text>
-                    <S.Name>@Bogdan</S.Name>
-                </S.Slide>
-            </FlexWrapper>
-            <S.Pagination>
-                <span></span>
-                <span className='Active'></span>
-                <span></span>
-            </S.Pagination>
-        </S.StyledSlider>
     );
-};*/
-
