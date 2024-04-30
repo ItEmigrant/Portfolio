ab2dc31e4ce9f0ccafc92cb1cde971bcc8a91db9
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-04-30 20:31:26 +0200

    part4reactAwesomeEnd

--- src/loyaut/sections/skills/Skills.tsx
+++ src/loyaut/sections/skills/Skills.tsx
@@ -4,6 +4,7 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
 import {Container} from "../../../components/Container";
 import {S} from "./Skill_Style"
+import {Fade} from "react-awesome-reveal";
 
 const skillData = [
     {
@@ -43,13 +44,16 @@ export const Skills = () => {
             <Container>
                 <SectionTitle>My skills</SectionTitle>
                 <FlexWrapper wrap={'wrap'} justify={'space-between'}>
-                    {skillData.map((s, index) => {
-                        return <Skill
-                            iconId={s.iconId}
-                            key={index}
-                            title={s.title}
-                            text={s.text}/>
-                    })}
+                    <Fade cascade={true} damping={.2}>
+                        {skillData.map((s, index) => {
+                            return <Skill
+                                iconId={s.iconId}
+                                key={index}
+                                title={s.title}
+                                text={s.text}/>
+                        })}
+                    </Fade>
+
                 </FlexWrapper>
             </Container>
         </S.Skills>
