226589c03a95ef022b0f5e891c20513e48f96dac
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-05-09 10:41:48 +0200

    part4EmailJSFormEnd

--- src/loyaut/sections/contacts/Contacts.tsx
+++ src/loyaut/sections/contacts/Contacts.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {ElementRef, useRef} from 'react';
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
 import {S} from './Contacts_Styles'
+import emailjs from '@emailjs/browser';
 
 export const Contact: React.FC = () => {
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+        if (!form.current) return;
+        emailjs
+            .sendForm('service_gt50yzm', 'template_r4fgb62', form.current, {
+                publicKey: 'LeCwvPKLVn7x7pwZV',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+        e.target.reset()
+    };
+
     return (
         <S.Contacts id={'contact'}>
             <Container>
                 <SectionTitle>Contacts</SectionTitle>
-                <S.StyledForm>
-                    <S.StyledField placeholder={'Name'}/>
-                    <S.StyledField placeholder={'Subject'}/>
-                    <S.StyledField placeholder={'Type message'} as={'textarea'}/>
+                <S.StyledForm ref={form} onSubmit={sendEmail}>
+                    <S.StyledField required placeholder={'Name'} name={'user_name'}/>
+                    <S.StyledField required placeholder={'Subject'} name={'subject'}/>
+                    <S.StyledField required placeholder={'Email'} name={'email'}/>
+                    <S.StyledField required placeholder={'Type message'} as={'textarea'} name={'message'}/>
                     <Button type={'submit'}>SEND MESSAGE</Button>
                 </S.StyledForm>
             </Container>
